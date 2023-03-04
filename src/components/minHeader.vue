9d647fbb2cc5e4537eaab343d8120e752a84d968
Aminzy <1243410203@qq.com>
2023-03-04 13:32:46 +0800

    link

--- src/components/minHeader.vue
+++ src/components/minHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="minesweeper-header">
     <!-- <button @click="useTheme.setTheme">{{ comTheme }}</button> -->
+    <a href="https://github.com/aminzi7/vue_minesweeper">github</a>
   </div>
 </template>
 
@@ -20,6 +21,7 @@ onMounted(() => {
 
 <style scoped lang="less">
 .minesweeper-header {
+  padding-top: 50px;
   button {
     background-color: var(--Theme-light-color);
     border: none;
