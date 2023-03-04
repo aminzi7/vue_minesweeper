71ae24fd8202033d14307f5f0383d3a8129169f1
Aminzy <1243410203@qq.com>
2023-03-04 10:17:02 +0800

    能翻拍

--- src/components/minCentre.vue
+++ src/components/minCentre.vue
@@ -10,7 +10,7 @@
           backgroundColor: getBGC(block),
         }"
       >
-        <template v-if="block.revealed">
+        <template v-if="block.revealed || dev">
           <div v-if="block.mine">
             <MinIcon></MinIcon>
           </div>
@@ -27,9 +27,19 @@
 import { reactive } from "vue";
 import MinIcon from "@/components/minIcone.vue";
 
+let mineGenerated = false;
+let dev = true;
 function btnCLick(block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block);
+    mineGenerated = true;
+  }
   // 记录没翻拍
   block.revealed = true;
+  if (block.mine) {
+    alert("BOOOOOM!");
+  }
+  expendZ(block);
 }
 
 interface BlockState {
@@ -57,14 +67,29 @@ const state = reactive(
   )
 );
 
-function generateMines() {
+function generateMines(init: BlockState) {
   for (const raw of state) {
     for (const block of raw) {
-      block.mine = Math.random() < 0.3;
+      // 一开始确保点不了炸弹,边缘判断
+      if (Math.abs(init.x - block.x) <= 1) continue;
+      if (Math.abs(init.y - block.y) <= 1) continue;
+      block.mine = Math.random() < 0.2;
     }
   }
+  updateNumbers();
 }
 
+function expendZ(block: BlockState) {
+  if (block.adjacentMines) return;
+
+  getSiblings(block).forEach((s) => {
+    // 翻拍
+    if (!s.revealed) {
+      s.revealed = true;
+      expendZ(s);
+    }
+  });
+}
 const directions = [
   [1, 1],
   [1, 0],
@@ -80,13 +105,8 @@ function updateNumbers() {
   state.forEach((raw, y) => {
     raw.forEach((block, x) => {
       if (block.mine) return;
-      directions.forEach(([dx, dy]) => {
-        const x2 = x + dx;
-        const y2 = y + dy;
-        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) {
-          return;
-        }
-        if (state[y2][x2].mine) block.adjacentMines += 1;
+      getSiblings(block).forEach((b) => {
+        if (b.mine) block.adjacentMines += 1;
       });
     });
   });
@@ -118,8 +138,21 @@ function getBGC(block: BlockState) {
   return block.mine ? "#471818" : "transparent";
 }
 
-generateMines();
-updateNumbers();
+// generateMines();
+// updateNumbers();
+
+function getSiblings(block: BlockState) {
+  return directions
+    .map(([dx, dy]) => {
+      const x2 = block.x + dx;
+      const y2 = block.y + dy;
+      if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT) {
+        return undefined;
+      }
+      return state[y2][x2];
+    })
+    .filter(Boolean) as BlockState[];
+}
 </script>
 
 <style lang="less" scoped>
